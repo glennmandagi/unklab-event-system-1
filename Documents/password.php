a05cbe37a04393db8505c2c7f4f0c52723a23bc3
Harold Ulaen <36790656+heviot@users.noreply.github.com>
2019-10-16 14:17:09 +0800

    add password tutorial and update plan

---
+++ Documents/password.php
@@ -0,0 +1,14 @@
+<?php
+
+    $user_pass = 'MyYpass123';
+    echo $user_pass;
+
+    echo "<p>&nbsp;</p>";
+
+    $hashed_pass = password_hash($user_pass, PASSWORD_BCRYPT);
+    echo $hashed_pass;
+
+    echo "<p>&nbsp;</p>";
+    $encypted_pass = md5($user_pass);
+    echo $encypted_pass;
+?>
