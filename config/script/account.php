695d2a50416ccf79467327c0fff3175d4f7d8765
Harold Ulaen <36790656+heviot@users.noreply.github.com>
2019-10-27 17:08:06 +0800

    Add database file and create account

--- config/script/account.php
+++ config/script/account.php
@@ -1,11 +1,11 @@
 <?php
-    
+    $conn = mysqli_connect('localhost', 'root', '', 'ersystem');
+
     if (isset($_POST['login'])) {
-	   $conn = mysqli_connect('localhost', 'root', '', 'ersystem');
     
         $username = mysqli_real_escape_string($conn, $_POST["username"]);  
         $password = mysqli_real_escape_string($conn, $_POST["password"]);  
-        $query = "SELECT * FROM user WHERE user_name = '$username'";  
+        $query = "SELECT * FROM users WHERE user_name = '$username'";  
         // $cek_user = "SELECT * FROM user WHERE user_name = $user_name AND user_pass = $user_pass";
         $result = mysqli_query($conn,$query);
     
@@ -22,7 +22,15 @@
                           $_SESSION['username'] = $username;  
                           $_SESSION["is_logged_in"] = TRUE;
                           
-                          header("location:../../layout/admin/dashboard/view.php");  
+                          if ($username == 'admin') {
+                              //kalo admin masuk sini
+                              header("location:../../layout/admin/dashboard/view.php");       
+                          }
+                          else{
+                              //kalo user masuk sini
+                              header("location:../../layout/user/dashboard/view.php");       
+                          }
+                          
                         echo "masuk";
                      }  
                      else  
@@ -54,5 +62,62 @@
           session_destroy();
           $_SESSION["is_logged_in"] = FALSE;
           header("location:../../");
+     }
+
+     if (isset($_POST['create_account'])) {
+          $nim = mysqli_real_escape_string($conn, $_POST["nim"]);  
+          $username = mysqli_real_escape_string($conn, $_POST["username"]);  
+          $password = mysqli_real_escape_string($conn, $_POST["password"]);  
+
+          $query = "SELECT * FROM students WHERE stu_nim = '$nim'";
+          
+          $result = mysqli_query($conn,$query);
+
+          // print_r($result);
+          if(mysqli_num_rows($result) > 0){  
+               // echo("Masuk pak eko");
+               $query = "SELECT * FROM users WHERE user_name = '$username' or student_stu_nim = '$nim'";
+          
+               $result = mysqli_query($conn,$query);
+
+               // print_r($result);
+
+               //kalo nda ada username yang pake itu
+               if(mysqli_num_rows($result) > 0){
+                    echo '
+                    <script language = "javascript">
+                         window.alert("CREATING ACCOUNT FAILED: Use another username or NIM");
+                         window.location.href="../../layout/create_account/index.php";    
+                    </script>';
+               }
+               else{
+                    // echo("Hai orang baru");
+                    $hashed_pass = password_hash($password, PASSWORD_BCRYPT);
+                    $sql = "INSERT INTO users(user_name, user_pass, student_stu_nim, user_status) VALUES ('$username','$hashed_pass', $nim, TRUE)";
+                    if (mysqli_query($conn, $sql)) {
+                         // echo "New record created successfully";
+                         echo '
+                             <script language = "javascript">
+                                 window.alert("SUCCESS: Account created");
+                                 window.location.href="../../index.php";
+                             </script>';
+                         // header("location:../../layout/admin/add_event/add.php");  
+                     } else {
+                         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                     }
+               }
+               // $sql = "INSERT INTO ";
+          }
+          else{  
+               echo '
+                <script language = "javascript">
+                    window.alert("CREATING ACCOUNT FAILED: Please try again");
+                    window.location.href="../../layout/create_account/index.php";
+                </script>';
+               //  echo '<script>alert("Wrong User Details")</script>';  
+               //  header("location:../../index.php");  
+          } 
+
+
      }
 ?>
--- layout/login/index.php
+++ layout/login/index.php
@@ -1,3 +1,8 @@
+<?php
+    // include('config/script/server.php');
+    // start_server();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,13 +35,13 @@
 					<img src="assets/images/img-01.png" alt="IMG">
 				</div>
 
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" method="post" action="config/script/account.php">
 					<span class="login100-form-title">
 						Member Login
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
+					<div class="wrap-input100 validate-input" data-validate = "username is required">
+						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -44,7 +49,7 @@
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" name="password" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -52,7 +57,7 @@
 					</div>
 					
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button class="login100-form-btn" type="submit" name="login">
 							Login
 						</button>
 					</div>
@@ -67,7 +72,7 @@
 					</div>
 
 					<div class="text-center p-t-136">
-						<a class="txt2" href="#">
+						<a class="txt2" href="layout/create_account/index.php">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
