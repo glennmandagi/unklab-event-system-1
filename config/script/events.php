79fdb0528750bda500ff21700f327e6ed7d30942
Harold Ulaen <36790656+heviot@users.noreply.github.com>
2019-11-01 09:50:28 +0800

    Add fitur admin check student

--- config/script/events.php
+++ config/script/events.php
@@ -71,17 +71,56 @@ if (isset($_POST['update'])) {
 if (isset($_GET['info'])) {
     $id = $_GET['info'];
 
+    $capacity = 0;
+    $occupied = 0;
+
+
+    //load event to search capacity
     $results = mysqli_query($conn, "SELECT * FROM events WHERE ev_id = '$id'");
-    
-    $row = mysqli_fetch_array($results);
+    $row_event = mysqli_fetch_array($results);
+
+    $capacity = $row_event['ev_capacity']; //add capacity
+
+    //load event to search capacity
+    $results = mysqli_query($conn, "SELECT COUNT(students_stu_id) AS num_user FROM registrations WHERE events_ev_id = '$id'");
+    $row_regist = mysqli_fetch_array($results);
+    $occupied = $row_regist['num_user'];
 
     echo("Event id          : " .'<br>'. $id .'<br>'.'<br>');
-    echo("Event Title       : " .'<br>'. $row['ev_title'] .'<br>'.'<br>');
-    echo("Event Date        : " .'<br>'. $row['ev_date_start'] . " to " . $row['ev_date_end'] .'<br>'.'<br>');
-    echo("Event Price       : " .'<br>'. $row['ev_price'] .'<br>'.'<br>');
-    echo("Event Location    : " .'<br>'. $row['ev_location'] .'<br>'.'<br>');
-    echo("Event Description : " .'<br>'. $row['ev_desc'] .'<br>'.'<br>');
+    echo("Event Title       : " .'<br>'. $row_event['ev_title'] .'<br>'.'<br>');
+    echo("Event Date        : " .'<br>'. $row_event['ev_date_start'] . " to " . $row_event['ev_date_end'] .'<br>'.'<br>');
+    echo("Event Price       : " .'<br>'. $row_event['ev_price'] .'<br>'.'<br>');
+    echo("Event Location    : " .'<br>'. $row_event['ev_location'] .'<br>'.'<br>');
+    echo("Event Description : " .'<br>'. $row_event['ev_desc'] .'<br>'.'<br>');
+    echo("Event Capacity    : " .'<br>'. $capacity .'<br>'.'<br>');
+    echo("Seat Occupied     : " .'<br>'. $occupied .'<br>'.'<br>');
+    echo("Seat Left         : " .'<br>'. ($capacity - $occupied) .'<br>'.'<br>');
+
+    $results = mysqli_query($conn, "SELECT registrations.reg_code AS code, students.stu_first_name AS stu_name, registrations.reg_time AS reg_date FROM registrations INNER JOIN students ON registrations.students_stu_id = students.stu_id where events_ev_id ='$id'") ;
+
+    $no = 1;
+    echo '<table border = "1">';
+        echo '<tr>';
+            echo '<td>'. "NO" .'</td>';
+            echo '<td>'. "CODE" . '</td>';
+            echo '<td>'. "Name" . '</td>';
+            echo '<td>'. "Date Register" . '</td>';
+        echo '</tr>';
+    //load student name
+    while ($student_registered = mysqli_fetch_array($results)) {
+        echo '<tr>';
+            echo '<td>'. $no . '</td>';
+            echo '<td>'. $student_registered['code'] . '</td>';
+            echo '<td>'. $student_registered['stu_name'] . '</td>';
+            echo '<td>'. $student_registered['reg_date'] . '</td>';
+        echo '</tr>';
+        $no++;
+    }
+    echo '</table>';
+
     
+    
+
     
 }
 
